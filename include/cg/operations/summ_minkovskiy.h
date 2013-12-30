dcd758204da4d6973361131bba06711428be981c
BgdanB <kail-stail@mail.ru>
2013-12-30 18:40:39 +0400

    minkowskiy_summ small fix

--- include/cg/operations/summ_minkovskiy.h
+++ include/cg/operations/summ_minkovskiy.h
@@ -24,32 +24,22 @@ namespace cg
       auto obst_it = std::min_element(obst.begin(), obst.end());
       auto robot_it = std::min_element(robot.begin(), robot.end());
 
-      for (int i = 0; i < obst.size(); ++i) {
-
-
-         auto obst_it_next = obst.begin();
+      for (int i = 0; i < obst.size() + 1; ++i) {
+         auto obst_it_next = obst_it + 1;
          if (obst_it == (obst.end() - 1)) {
             obst_it_next = obst.begin();
-         } else {
-            obst_it_next = obst_it + 1;
          }
 
          for (int i = 0; i < obst.size() + robot.size(); ++i) {
-
-            auto robot_it_next = robot.begin();
+            auto robot_it_next = robot_it + 1;
             if (robot_it == (robot.end() - 1)) {
                robot_it_next = robot.begin();
-
-            } else {
-               robot_it_next = robot_it + 1;
             }
-
-
 
             point_2t<Scalar> point(obst_it->x + robot_it->x, obst_it->y + robot_it->y);
 
             if (ans.size() > 1 ) {
-               if (orientation(ans[ans.size() - 1], ans[ans.size() - 2], point) == CG_COLLINEAR) {
+               if (orientation(ans[ans.size() - 2], ans[ans.size() - 1], point) == CG_COLLINEAR) {
                   ans.pop_back();
                   ans.push_back(point);
                } else {
@@ -59,14 +49,12 @@ namespace cg
                ans.push_back(point);
             }
 
-            printf(" %.2f %.2f\n", ans.back().x, ans.back().y);
-
             Scalar delta_x = robot_it_next->x - robot_it->x;
             Scalar delta_y = robot_it_next->y - robot_it->y;
 
             point_2t<Scalar> new_obst_point(obst_it_next->x + delta_x, obst_it_next->y + delta_y);
 
-            if (orientation(*obst_it, *obst_it_next, new_obst_point) == CG_LEFT) {
+            if (orientation(*obst_it, *obst_it_next, new_obst_point) != CG_RIGHT) {
                break;
             } else {
                robot_it = robot_it_next;
@@ -79,7 +67,6 @@ namespace cg
       if (ans.size() > 1 && cg::contains(segment_2t<Scalar>(ans[ans.size() - 2], ans[0]), ans[ans.size() - 1])) {
          ans.pop_back();
       }
-
       return ans;
    }
 
